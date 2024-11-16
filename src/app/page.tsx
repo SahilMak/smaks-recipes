6f965f85b288e5cd562b23b03866420e7dce6eaa
Sahil Makhijani <svmakhijani94@gmail.com>
2024-11-16 15:27:49 -0700

    Added background image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import styles from './page.module.scss'
+import BgImage from '@/components/bgImage/BgImage';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>Home</div>
+		<BgImage/>
+		<h2>Home</h2>
     </main>
   )
 }

af3177f1b9102040732f1fd1b12c75b9ac6adfee
Sahil Makhijani <svmakhijani94@gmail.com>
2024-11-17 12:10:43 -0700

    Moved navbar menu links into designated file

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -16,37 +16,9 @@ import Typography from '@mui/material/Typography';
 import Zoom from '@mui/material/Zoom';
 import { useColorScheme } from '@mui/material/styles';
 import Search from './Search';
+import { cuisine, meals, pages } from './menuLinks';
 import { gayathri, shrikhand } from '@/fonts';
 import './navbar.scss';
-
-const pages = [
-	{text: 'HOME', link: '/'},
-	{text: 'MEALS', link: ''},
-	{text: 'CUISINE', link: ''},
-	{text: 'ABOUT', link: '/about'}
-];
-const meals = [
-	{text: 'Beverages', link: '/meals/beverages'},
-	{text: 'Breakfast', link: '/meals/breakfast'},
-	{text: 'Dessert', link: '/meals/dessert'},
-	{text: 'Dinner', link: '/meals/dinner'},
-	{text: 'Lunch', link: '/meals/lunch'},
-	{text: 'Sides', link: '/meals/sides'},
-	{text: 'Soup', link: '/meals/soup'},
-	{text: 'View all', link: '/meals'}
-];
-const cuisine = [
-	{text: 'American', link: '/cuisine/american'},
-	{text: 'Asian', link: '/cuisine/asian'},
-	{text: 'Filipino', link: '/cuisine/filipino'},
-	{text: 'Greek', link: '/cuisine/greek'},
-	{text: 'Indian', link: '/cuisine/indian'},
-	{text: 'Indo-Chinese', link: '/cuisine/indo-chinese'},
-	{text: 'Lebanese', link: '/cuisine/lebanese'},
-	{text: 'Mexican', link: '/cuisine/mexican'},
-	{text: 'Thai', link: '/cuisine/thai'},
-	{text: 'View all', link: '/cuisine'}
-];
 
 export default function Navbar() {
 	const { mode, setMode } = useColorScheme();
---
+++ src/components/navbar/menuLinks.ts
@@ -0,0 +1,30 @@
+export const cuisine = [
+	{text: 'American', link: '/cuisine/american'},
+	{text: 'Asian', link: '/cuisine/asian'},
+	{text: 'Filipino', link: '/cuisine/filipino'},
+	{text: 'Greek', link: '/cuisine/greek'},
+	{text: 'Indian', link: '/cuisine/indian'},
+	{text: 'Indo-Chinese', link: '/cuisine/indo-chinese'},
+	{text: 'Lebanese', link: '/cuisine/lebanese'},
+	{text: 'Mexican', link: '/cuisine/mexican'},
+	{text: 'Thai', link: '/cuisine/thai'},
+	{text: 'View all', link: '/cuisine'}
+];
+
+export const meals = [
+	{text: 'Beverages', link: '/meals/beverages'},
+	{text: 'Breakfast', link: '/meals/breakfast'},
+	{text: 'Dessert', link: '/meals/dessert'},
+	{text: 'Dinner', link: '/meals/dinner'},
+	{text: 'Lunch', link: '/meals/lunch'},
+	{text: 'Sides', link: '/meals/sides'},
+	{text: 'Soup', link: '/meals/soup'},
+	{text: 'View all', link: '/meals'}
+];
+
+export const pages = [
+	{text: 'HOME', link: '/'},
+	{text: 'MEALS', link: ''},
+	{text: 'CUISINE', link: ''},
+	{text: 'ABOUT', link: '/about'}
+];

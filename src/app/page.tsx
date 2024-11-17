21c459102f3d23f852ef002f47de787b5d832b61
Sahil Makhijani <svmakhijani94@gmail.com>
2024-11-16 19:46:16 -0700

    Added placeholder text to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,32 @@
 import * as React from 'react';
 import styles from './page.module.scss'
 import BgImage from '@/components/bgImage/BgImage';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 export default function Home() {
   return (
     <main className={styles.main}>
 		<BgImage/>
-		<h2>Home</h2>
+		<Stack
+			className={styles.stack}
+			spacing={2}
+		>
+			<Typography
+				className={styles.typographyTop}
+				component="div"
+				variant="h2"
+			>
+				Welcome to<br />Smak&apos;s Recipes!
+			</Typography>
+			<Typography
+				className={styles.typographyBottom}
+				component="div"
+				variant="h2"
+			>
+				Coming soon
+			</Typography>
+		</Stack>
     </main>
   )
 }

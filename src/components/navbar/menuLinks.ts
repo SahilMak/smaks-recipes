eb9231bc19afeb8bd92091bbf347859da02d4054
Sahil Makhijani <svmakhijani94@gmail.com>
2024-11-17 12:42:37 -0700

    Turned navbar menu links into query params

--- src/components/navbar/menuLinks.ts
+++ src/components/navbar/menuLinks.ts
@@ -1,24 +1,24 @@
 export const cuisine = [
-	{text: 'American', link: '/cuisine/american'},
-	{text: 'Asian', link: '/cuisine/asian'},
-	{text: 'Filipino', link: '/cuisine/filipino'},
-	{text: 'Greek', link: '/cuisine/greek'},
-	{text: 'Indian', link: '/cuisine/indian'},
-	{text: 'Indo-Chinese', link: '/cuisine/indo-chinese'},
-	{text: 'Lebanese', link: '/cuisine/lebanese'},
-	{text: 'Mexican', link: '/cuisine/mexican'},
-	{text: 'Thai', link: '/cuisine/thai'},
+	{text: 'American', link: '/cuisine?c=american'},
+	{text: 'Asian', link: '/cuisine?c=asian'},
+	{text: 'Filipino', link: '/cuisine?c=filipino'},
+	{text: 'Greek', link: '/cuisine?c=greek'},
+	{text: 'Indian', link: '/cuisine?c=indian'},
+	{text: 'Indo-Chinese', link: '/cuisine?c=indo-chinese'},
+	{text: 'Lebanese', link: '/cuisine?c=lebanese'},
+	{text: 'Mexican', link: '/cuisine?c=mexican'},
+	{text: 'Thai', link: '/cuisine?c=thai'},
 	{text: 'View all', link: '/cuisine'}
 ];
 
 export const meals = [
-	{text: 'Beverages', link: '/meals/beverages'},
-	{text: 'Breakfast', link: '/meals/breakfast'},
-	{text: 'Dessert', link: '/meals/dessert'},
-	{text: 'Dinner', link: '/meals/dinner'},
-	{text: 'Lunch', link: '/meals/lunch'},
-	{text: 'Sides', link: '/meals/sides'},
-	{text: 'Soup', link: '/meals/soup'},
+	{text: 'Beverages', link: '/meals?m=beverages'},
+	{text: 'Breakfast', link: '/meals?m=breakfast'},
+	{text: 'Dessert', link: '/meals?m=dessert'},
+	{text: 'Dinner', link: '/meals?m=dinner'},
+	{text: 'Lunch', link: '/meals?m=lunch'},
+	{text: 'Sides', link: '/meals?m=sides'},
+	{text: 'Soup', link: '/meals?m=soup'},
 	{text: 'View all', link: '/meals'}
 ];
 
